3bb3d3d512e995f26cd511557be64bb3a521a648
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-09-10 09:03:20 -0300

    instegrando sass
    asd

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "/src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
-      title: "Development",
+      title: "Vue 3 webpack",
+      template: 'public/index.html'
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
     ],
   },
   output: {
-    path: path.join(__dirname, "public" + "js"),
+    path: path.join(__dirname, "dist"),
     filename: "bundle.js",
   },
 };
