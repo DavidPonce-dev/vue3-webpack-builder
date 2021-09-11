2b0fb3f685c838c87af0976d70b6f681ecf2af3d
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-09-11 06:40:06 -0300

    agregando open para abrir navegador al arrancar script

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,45 @@
 const path = require("path");
-const { DefinePlugin } = require('webpack')
-const { VueLoaderPlugin } = require('vue-loader')
+const { DefinePlugin } = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const open = require("open");
 
-module.exports = {
-  entry: "/src/index.js",
-  plugins: [
-    new DefinePlugin({
-      __VUE_OPTIONS_API__: false,
-      __VUE_PROD_DEVTOOLS__: false,
-    }),
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Vue 3 webpack",
-      template: 'public/index.html'
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
+module.exports = (env, argv) => {
+  
+  if (argv.mode === 'development') open("http://localhost:8080");
+
+  return {
+    entry: "/src/index.js",
+    plugins: [
+      new DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        title: "Vue 3 webpack",
+        template: "public/index.html",
+      }),
     ],
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+  };
 };
