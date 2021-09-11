93b3589ee29816e9f01ab1ab86f4ec514e7048d4
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-09-11 06:44:07 -0300

    prueba de script y css

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>vue app</h1>
+  <div class="app">
+  <button @click="()=>{addCount()}">Counter: {{counter}}</button>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    counter: 0
+  }),
 
+  methods: {
+    addCount () {
+      this.counter++
+    }
+  }
 }
 </script>
 
 <style>
-
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  #app{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #000;
+  }
+  button{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    font-size: 30px;
+    background: #050;
+    color: #fff;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  }
 </style>
